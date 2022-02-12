00e0c9cf8f99fff2a4543f6b1a018203eeeec9f3
alex <camward@yandex.ru>
2022-02-12 21:31:17 +0400

    сохранение токена. работа с файловой системой

--- weather.js
+++ weather.js
@@ -1,13 +1,29 @@
 #!/usr/bin/env node
 
 import { getArgs } from "./helpers/args.js";
-import { printHelp } from "./services/log.service.js";
+import { printHelp, printSuccess, printError } from "./services/log.service.js";
+import { saveKeyValue } from "./services/storage.service.js";
+
+const saveToken = async (token) => {
+  if (!token.length) {
+    printError("Не передан token");
+    return;
+  }
+  try {
+    await saveKeyValue("token", token);
+    printSuccess("Токен сохранён");
+  } catch (e) {
+    printError(e.message);
+  }
+};
 
 const initCLI = () => {
   const args = getArgs(process.argv);
-  console.log("init", args);
   if (args.h) {
     return printHelp();
+  }
+  if (args.t) {
+    return saveToken(args.t);
   }
 };
 
