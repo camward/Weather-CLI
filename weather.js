17c1c7a6bb994aeed5f66ea220dd3141d6f0c810
alex <camward@yandex.ru>
2022-02-13 18:06:17 +0400

    взаимодействие с API

--- weather.js
+++ weather.js
@@ -2,7 +2,7 @@
 
 import { getArgs } from "./helpers/args.js";
 import { printHelp, printSuccess, printError } from "./services/log.service.js";
-import { saveKeyValue } from "./services/storage.service.js";
+import { saveKeyValue, TOKEN_DICTIONARY } from "./services/storage.service.js";
 
 const saveToken = async (token) => {
   if (!token.length) {
@@ -10,7 +10,7 @@ const saveToken = async (token) => {
     return;
   }
   try {
-    await saveKeyValue("token", token);
+    await saveKeyValue(TOKEN_DICTIONARY.token, token);
     printSuccess("Токен сохранён");
   } catch (e) {
     printError(e.message);
